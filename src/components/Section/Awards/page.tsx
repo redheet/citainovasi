77273702182dc4a680809b7a93c99c89635ef3dc
redheet <naufalzidane32@gmail.com>
2025-05-09 10:41:32 +0700

    img change to next/image

--- src/components/Section/Awards/page.tsx
+++ src/components/Section/Awards/page.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 const Awards = () => {
   return (
     <section className="text-white style-six lg:py-[100px] sm:py-16 py-10">
@@ -12,14 +14,14 @@ const Awards = () => {
               <div className="service-item box-shadow-none">
                 <div className="service-item-main flex flex-col gap-4">
                   <i className="text-4xl">
-                    <img src="/images/awards/juara-1.png" alt="" />
+                    <Image src="/images/awards/juara-1.png" alt="" width={150} height={150}/>
                   </i>
                   <div className="desc">
                     <div className="heading7 hover-text-blue">Gold Medal</div>
-                    <div className="body3 text-white mt-1">
+                    {/* <div className="body3 text-white mt-1">
                       Experience the convenience of banking with easy access to
                       your accounts anytime, anywhere.
-                    </div>
+                    </div> */}
                   </div>
                 </div>
               </div>
@@ -28,14 +30,14 @@ const Awards = () => {
               <div className="service-item box-shadow-none">
                 <div className="service-item-main flex flex-col gap-4">
                   <i className="text-4xl">
-                    <img src="/images/awards/juara-2.png" alt="" />
+                    <Image src="/images/awards/juara-2.png" alt="" width={150} height={150}/>
                   </i>
                   <div className="desc">
                     <div className="heading7 hover-text-blue">Silver Medal</div>
-                    <div className="body3 text-white mt-1">
+                    {/* <div className="body3 text-white mt-1">
                       Rest assured with our robust security measures to protect
                       your transactions and sensitive.
-                    </div>
+                    </div> */}
                   </div>
                 </div>
               </div>
@@ -44,17 +46,17 @@ const Awards = () => {
               <div className="service-item box-shadow-none">
                 <div className="service-item-main flex flex-col gap-4">
                   <i className="text-4xl">
-                    <img src="/images/awards/juara-3.png" alt="" />
+                    <Image src="/images/awards/juara-3.png" alt="" width={150} height={150} />
                   </i>
                   <div className="desc">
                     <div className="heading7 hover-text-blue">
                       {" "}
                       Bronze Medal
                     </div>
-                    <div className="body3 text-white mt-1">
+                    {/* <div className="body3 text-white mt-1">
                       Stay in control of your finances with effortless tracking
                       and monitoring of your transactions.
-                    </div>
+                    </div> */}
                   </div>
                 </div>
               </div>
@@ -63,16 +65,16 @@ const Awards = () => {
               <div className="service-item box-shadow-none">
                 <div className="service-item-main flex flex-col gap-4">
                   <i className="text-4xl">
-                    <img src="/images/awards/honorable-mention.png" alt="" />
+                    <Image src="/images/awards/honorable-mention.png" alt="" width={150} height={150} />
                   </i>
                   <div className="desc">
                     <div className="heading7 hover-text-blue">
                       Honorable Mention
                     </div>
-                    <div className="body3 text-white mt-1">
+                    {/* <div className="body3 text-white mt-1">
                       Enjoy round-the-clock access to your accounts for tracking
                       seamless financial management.
-                    </div>
+                    </div> */}
                   </div>
                 </div>
               </div>
