7b56307b0fcda3ad7f403f8fcac457666faf345e
redheet <naufalzidane32@gmail.com>
2025-05-09 14:38:25 +0700

    logo image

--- src/components/Header/Menu/MenuSix.tsx
+++ src/components/Header/Menu/MenuSix.tsx
@@ -39,11 +39,11 @@ const MenuSix = () => {
         <div className="container">
           <div className="header-main">
             <div className="main flex items-center justify-between lg:h-20 h-16 py-4 px-7 rounded-full">
-              <Link className="menu-left-block" href="/">
+              <Link className="menu-left-block" href="/homepages/gsic">
                 <Image
                   src={"/images/Logo.webp"}
-                  width={1800}
-                  height={1600}
+                  width={1600}
+                  height={441}
                   alt="logo"
                   priority={true}
                   className="w-[149px] max-sm:w-[128px]"
