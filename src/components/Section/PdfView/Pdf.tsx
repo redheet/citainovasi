3bbaa1f2d9cceabda62fd64c041a0738a955996f
redheet <naufalzidane32@gmail.com>
2025-05-22 11:00:03 +0700

    update pdf flipbook

--- src/components/Section/PdfView/Pdf.tsx
+++ src/components/Section/PdfView/Pdf.tsx
@@ -1,37 +1,31 @@
 import React from 'react'
-import FlipBook from '@/components/Section/PdfView/Flipbook';
+// import FlipBook from '@/components/Section/PdfView/Flipbook';
 
 export default function Pdf() {
-    const pages = [
-        "/images/guidebook/1.png",
-        "/images/guidebook/2.png",
-        "/images/guidebook/3.png",
-        "/images/guidebook/4.png",
-        "/images/guidebook/5.png",
-        "/images/guidebook/6.png",
-        "/images/guidebook/7.png",
-        "/images/guidebook/8.png",
-        "/images/guidebook/9.png",
-        "/images/guidebook/10.png",
-        "/images/guidebook/11.png",
-        "/images/guidebook/12.png",
-        "/images/guidebook/15.png",
-        "/images/guidebook/16.png",
-        "/images/guidebook/17.png",
-        "/images/guidebook/18.png",
-        "/images/guidebook/19.png",
+    // const pages = [
+    //     "/images/guidebook/1.png",
+    //     "/images/guidebook/2.png",
+    //     "/images/guidebook/3.png",
+    //     "/images/guidebook/4.png",
+    //     "/images/guidebook/5.png",
+    //     "/images/guidebook/6.png",
+    //     "/images/guidebook/7.png",
+    //     "/images/guidebook/8.png",
+    //     "/images/guidebook/9.png",
+    //     "/images/guidebook/10.png",
+    //     "/images/guidebook/11.png",
+    //     "/images/guidebook/12.png",
+    //     "/images/guidebook/15.png",
+    //     "/images/guidebook/16.png",
+    //     "/images/guidebook/17.png",
+    //     "/images/guidebook/18.png",
+    //     "/images/guidebook/19.png",
         
-        // ... more images
-      ];
+    //     // ... more images
+    //   ];
   return (
-    <div>
-      <FlipBook 
-        pages={pages}
-        title="Guide Book GSIC"
-        endTitle="END Guide Book"
-        width={90}
-        height={100}
-      />
+    <div className='mt-10'>
+      <iframe allowFullScreen scrolling="no" className="fp-iframe border-0" src="https://heyzine.com/flip-book/cd61061f43.html" width= "100%" height= "600px"></iframe>
     </div>
   )
 }
