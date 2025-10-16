c4892c386d48f5f38a6073f9646c9a87df40b339
redheet <naufalzidane32@gmail.com>
2025-10-16 12:12:27 +0700

    comment component not used

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -43,28 +43,29 @@
 import TopNavOne from "@/components/Header/TopNav/TopNavOne";
 import MenuSix from "@/components/Header/Menu/MenuSix";
 import SliderSix from "@/components/Slider/SliderSix";
-import PartnerSix from "@/components/Section/Partner/PartnerSix";
+// import PartnerSix from "@/components/Section/Partner/PartnerSix";
 import CounterSix from "@/components/Section/Counter/CounterSix";
-import serviceData from '@/data/service.json'
-import regisData from '@/data/regis.json'
+import serviceData from "@/data/service.json";
+import regisData from "@/data/regis.json";
 import ServiceSix from "@/components/Section/Service/ServiceSix";
-import AboutSix from "@/components/Section/About/AboutSix";
-import PricingSix from "@/components/Section/Pricing/PricingSix";
-import ServiceSixSecond from "@/components/Section/Service/ServiceSixSecond";
-import testimonialData from '@/data/testimonial.json'
-import TestimonialSix from "@/components/Section/Testimonial/TestimonialSix";
-import blogData from '@/data/blog.json'
-import BlogSix from "@/components/Section/Blog/BlogSix";
+// import AboutSix from "@/components/Section/About/AboutSix";
+// import PricingSix from "@/components/Section/Pricing/PricingSix";
+// import ServiceSixSecond from "@/components/Section/Service/ServiceSixSecond";
+// import testimonialData from "@/data/testimonial.json";
+// import TestimonialSix from "@/components/Section/Testimonial/TestimonialSix";
+// import blogData from "@/data/blog.json";
+// import BlogSix from "@/components/Section/Blog/BlogSix";
 import Footer from "@/components/Footer/Footer";
-import FormRequestSix from "@/components/Section/FormRequest/FormRequestSix";
+// import FormRequestSix from "@/components/Section/FormRequest/FormRequestSix";
 import Awards from "@/components/Section/Awards/page";
 import RegistrationRules from "@/components/Section/Service/RegistrationRules";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-    title: "GSIC - Global Sustainability Innovation Competition",
-    description: "Global Sustainability Innovation Competition (GSIC) is a Prominent platform dedicated to fostering and recognizing innovators who are at the forefront of creating impactful solutions for sustainability.",
-  };
+  title: "GSIC - Global Sustainability Innovation Competition",
+  description:
+    "Global Sustainability Innovation Competition (GSIC) is a Prominent platform dedicated to fostering and recognizing innovators who are at the forefront of creating impactful solutions for sustainability.",
+};
 
 export default function HomeSix() {
   return (
